fe493254646886cbbf62c703d8e36617765ac833
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-06 20:21:04 +0300

    add lower stack frame and find blocked tasks

--- src/task/task.c
+++ src/task/task.c
@@ -1,5 +1,9 @@
 #include <stdint.h>
 #include "task/task.h"
+#include "uart/pl011.h"
+#include "systick/systick.h"
+
+#define ICSR_REG (*((volatile uint32_t*) ICSR))
 
 TCB_t *task_table[MAX_TASKS]; 
 uint8_t task_count = 0; 
@@ -12,16 +16,26 @@ void task_create(TCB_t *tcb, void (*entry)(void), uint8_t task_id)
     tcb->task_state = READY;
 
     uint32_t *sp = &(tcb->stack[256]);
+
     *(--sp) = 0x01000000;        
-    *(--sp) = (uint32_t) entry;  
-    *(--sp) = 0xFFFFFFF9;        
+    *(--sp) = (uint32_t)entry;   
+    *(--sp) = 0xFFFFFFFD;        
     *(--sp) = 0;                 
     *(--sp) = 0;                 
-    *(--sp) = 0;                
-    *(--sp) = 0;                
+    *(--sp) = 0;                 
+    *(--sp) = 0;                 
     *(--sp) = 0;                 
 
-    tcb->stack_pointer = sp;
+    *(--sp) = 0;                 
+    *(--sp) = 0;                 
+    *(--sp) = 0;                 
+    *(--sp) = 0;                 
+    *(--sp) = 0;                 
+    *(--sp) = 0;                 
+    *(--sp) = 0;             
+    *(--sp) = 0;                
+
+    tcb->stack_pointer = sp;     
 }
 
 void task_register(TCB_t *tcb) 
@@ -33,12 +47,27 @@ void task_register(TCB_t *tcb)
     }
 }
 
-void scheduler_next(void) 
+void scheduler_next(void)
 {
-    current_task_index++;
-
-    if (current_task_index >= task_count)
+    for (uint8_t i = 0; i < task_count; i++)
     {
-        current_task_index = 0;
+        current_task_index++;
+
+        if (current_task_index >= task_count) 
+        {
+            current_task_index = 0;
+        }
+
+        TCB_t *tcb = task_table[current_task_index];
+
+        if (tcb->task_state == BLOCKED && global_tick_counter >= tcb->wake_tick)
+        {
+            tcb->task_state = READY;
+        }
+
+        if (tcb->task_state == READY)
+        {
+            return;
+        }
     }
 }
