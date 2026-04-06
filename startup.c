a72c11633fbd7a2383b8d0a9bd90baa2f332c857
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-06 12:52:21 +0300

    fill out vector table entries

--- startup.c
+++ startup.c
@@ -1,6 +1,14 @@
 #include <stdint.h>
 
 void Reset_Handler(void);
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void MemManage_Handler(void);
+void BusFault_Handler(void);
+void UsageFault_Handler(void);
+void SVC_Handler(void);
+void DebugMon_Handler(void);
+void PendSV_Handler(void);
 void SysTick_Handler(void);
 
 extern uint32_t _stack_top, _data_flash_start, _data_start, _data_end, _bss_start, _bss_end;
@@ -8,23 +16,31 @@ extern void main(void);
 
 const void * const _Vectors[] __attribute__((section(".isr_vector"))) =
 {
-    &_stack_top,       
-    Reset_Handler,    
-    0,                 
-    0,               
-    0,                 
-    0,                 
-    0,              
-    0,                 
-    0,               
-    0,                 
-    0,                 
-    0,                
-    0,                
-    0,            
-    0,               
-    SysTick_Handler,   
+    &_stack_top,
+    Reset_Handler,
+    NMI_Handler,
+    HardFault_Handler,
+    MemManage_Handler,
+    BusFault_Handler,
+    UsageFault_Handler,
+    0,
+    0,
+    0,
+    0,
+    SVC_Handler,
+    DebugMon_Handler,
+    0,
+    PendSV_Handler,
+    SysTick_Handler,
 };
+
+void NMI_Handler(void) { while (1); }
+void HardFault_Handler(void) { while (1); }
+void MemManage_Handler(void) { while (1); }
+void BusFault_Handler(void) { while (1); }
+void UsageFault_Handler(void) { while (1); }
+void SVC_Handler(void) { while (1); }
+void DebugMon_Handler(void) { while (1); }
 
 void Reset_Handler(void)
 {
