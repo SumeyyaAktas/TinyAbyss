08f1cca0b00d4a7a4c2844a3edd6f535eb7e6d06
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-07 12:45:34 +0300

    use struct for register addresses

--- include/systick/systick.h
+++ include/systick/systick.h
@@ -3,16 +3,27 @@
 
 #include <stdint.h>
 
-#define SYST_CSR 0xE000E010
-#define SYST_RVR 0xE000E014
-#define SYST_CVR 0xE000E018
+typedef struct
+{
+    volatile uint32_t CSR;  
+    volatile uint32_t RVR;  
+    volatile uint32_t CVR;  
+} SysTick_t;
+
+typedef struct
+{
+    volatile uint32_t RESERVED0;
+    volatile uint32_t ICSR;
+} SCB_t;
+
+#define SYSTICK ((SysTick_t *) 0xE000E010)
+#define SCB ((SCB_t *) 0xE000ED00)
 
 #define CSR_ENABLE (1 << 0)
 #define CSR_TICKINT (1 << 1)
 #define CSR_CLKSOURCE (1 << 2)
 
-#define ICSR 0xE000ED04 
-#define ICSR_PENDSVSET (1 << 28) 
+#define ICSR_PENDSVSET (1 << 28)
 
 extern volatile uint32_t global_tick_counter;
 
--- src/systick/systick.c
+++ src/systick/systick.c
@@ -1,25 +1,20 @@
 #include "systick/systick.h"
 #include "config.h"
 
-#define SYST_CSR_REG (*((volatile uint32_t*) SYST_CSR))
-#define SYST_RVR_REG (*((volatile uint32_t*) SYST_RVR))
-#define SYST_CVR_REG (*((volatile uint32_t*) SYST_CVR))
-#define ICSR_REG (*((volatile uint32_t*) ICSR))
-
 volatile uint32_t global_tick_counter = 0;
 
 void SysTick_Handler(void) 
 {
     global_tick_counter++;
-    
-    ICSR_REG |= ICSR_PENDSVSET;
+
+    SCB->ICSR |= ICSR_PENDSVSET;
 }
 
 void systick_init(void)
 {
-    SYST_CSR_REG = 0;
-    SYST_RVR_REG = (CLOCK_HZ / TICK_HZ) - 1;
-    SYST_CVR_REG = 0;
+    SYSTICK->CSR = 0;
+    SYSTICK->RVR = (CLOCK_HZ / TICK_HZ) - 1;
+    SYSTICK->CVR = 0;
 
-    SYST_CSR_REG = CSR_ENABLE | CSR_TICKINT | CSR_CLKSOURCE;
+    SYSTICK->CSR = CSR_ENABLE | CSR_TICKINT | CSR_CLKSOURCE;
 }
