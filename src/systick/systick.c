a55fcac48840c1e8220aa6597f210491036d7e70
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-07 12:03:17 +0300

    include config

--- src/systick/systick.c
+++ src/systick/systick.c
@@ -1,4 +1,5 @@
 #include "systick/systick.h"
+#include "config.h"
 
 #define SYST_CSR_REG (*((volatile uint32_t*) SYST_CSR))
 #define SYST_RVR_REG (*((volatile uint32_t*) SYST_RVR))
@@ -17,7 +18,7 @@ void SysTick_Handler(void)
 void systick_init(void)
 {
     SYST_CSR_REG = 0;
-    SYST_RVR_REG = 11999;
+    SYST_RVR_REG = (CLOCK_HZ / TICK_HZ) - 1;
     SYST_CVR_REG = 0;
 
     SYST_CSR_REG = CSR_ENABLE | CSR_TICKINT | CSR_CLKSOURCE;
