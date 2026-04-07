6d844c6d592924fb894351e311397e92ea692ebf
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-07 12:18:29 +0300

    apply config and add delay_ms function

--- include/task/task.h
+++ include/task/task.h
@@ -2,8 +2,7 @@
 #define TASK_H
 
 #include <stdint.h>
-
-#define MAX_TASKS 2 
+#include "config.h"
 
 typedef enum 
 {
@@ -12,24 +11,25 @@ typedef enum
     BLOCKED
 } States;
 
-typedef struct 
+typedef struct TCB
 {
     uint32_t *stack_pointer;
     void (*entry)(void);
-    uint32_t stack[256];
+    uint32_t stack[STACK_SIZE];
     uint8_t task_id;
     States task_state;
     uint32_t wake_tick;
 } TCB_t;
 
-extern TCB_t *task_table[MAX_TASKS]; 
-extern uint8_t task_count; 
-extern uint8_t current_task_index; 
+extern TCB_t *task_table[MAX_TASKS];
+extern uint8_t task_count;
+extern uint8_t current_task_index;
 
 void task_create(TCB_t *tcb, void (*entry)(void), uint8_t task_id);
 void task_register(TCB_t *tcb); 
 void scheduler_next(void); 
 void task_start(void);
 void delay(uint32_t ticks);
+void delay_ms(uint32_t ms);
 
-#endif 
+#endif
