1ef97c9b72818cbf08a9ac1da3202f16724cb027
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-13 12:58:02 +0300

    implement mutex unlock

--- src/mutex/mutex.c
+++ src/mutex/mutex.c
@@ -31,3 +31,36 @@ TCB_t *mutex_dequeue(Mutex_t *m)
     return tcb;
 }
 
+void mutex_unlock(Mutex_t *m)
+{
+    __asm__ volatile("cpsid i");
+
+    TCB_t *current = task_table[current_task_index];
+
+    if (m->owner != current->task_id) 
+    {
+        __asm__ volatile("cpsie i");
+        return;
+    }
+
+    current->effective_priority = current->base_priority;
+
+    TCB_t *next_waiter = mutex_dequeue(m);
+
+    if (next_waiter != NULL)
+    {
+        m->owner  = next_waiter->task_id;
+        m->locked = 1;
+        m->holder = next_waiter;
+        next_waiter->task_state  = READY;
+        next_waiter->waiting_on  = NULL;
+    }
+    else
+    {
+        m->locked = 0;
+        m->owner  = -1;
+        m->holder = NULL;
+    }
+
+    __asm__ volatile("cpsie i");
+}
